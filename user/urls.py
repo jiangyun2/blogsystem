aae4bb0a86dd41615023a52df330b9b5b988fd06
jiangyun2 <570454176@qq.com>
2019-07-22 19:10:16 +0800

    user模板渲染

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,11 @@
 
 from django.urls import path, include
 from . import views
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='user_index'),
+    path('register', views.UserRegister.as_view(), name='user_register'),
+    path('login', views.UserLogin.as_view(), name='user_login'),
+    path('updatepassword', views.UserUpdatePassword.as_view(), name='user_updatepassword'),
+    path('logout', views.user_logout, name='user_logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 # Create your views here.
-
+from django.views import View
 
 def index(request):
     return HttpResponse('user_index')
+
+
+class UserRegister(View):
+    def get(self, request):
+        return render(request, 'user/user_register.html')
+
+    def post(self,request):
+        return render(request, 'user/user_register.html')
+
+
+class UserLogin(View):
+    def get(self, request):
+        return render(request, 'user/user_login.html')
+
+    def post(self, request):
+        return render(request, 'user/user_login.html')
+
+
+class UserUpdatePassword(View):
+    def get(self, request):
+        return render(request, 'user/user_updatepassword.html')
+
+    def post(self, request):
+        return render(request, 'user/user_updatepassword.html')
+
+
+def user_logout(request):
+    return redirect(reverse('user_index'))
+
