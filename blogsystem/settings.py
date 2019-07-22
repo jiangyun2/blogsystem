199bdb6e311b37e872a2aa0308a342cd7d923209
jiangyun2 <570454176@qq.com>
2019-07-22 19:23:03 +0800

    mysql数据库配置 数据库创建

--- blogsystem/settings.py
+++ blogsystem/settings.py
@@ -75,10 +75,17 @@ WSGI_APPLICATION = 'blogsystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# 数据库配置
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogsystem',
+        'USER': 'admin',
+        'PASSWORD': 'Root110qwe',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- user/models.py
+++ user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BlogUser(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return 'BlogUser<usename={},password={},email={}>'.format(self.username, self.password, self.email)
+
+
+class BlogContent(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return 'BlogContent<title={}, content={}>'.format(self.title, self.content)
