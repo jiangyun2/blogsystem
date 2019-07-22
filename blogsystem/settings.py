43b92ce537253287ffa49bea7ea193e507b4333b
jiangyun2 <570454176@qq.com>
2019-07-22 18:49:51 +0800

    新建app 注册app

--- blogsystem/settings.py
+++ blogsystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^-smlq6eba(&so-3netomlwmmzf^d_&^+i(@!f2s65=yq1i2_('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # app注册
+    'user',
 ]
 
 MIDDLEWARE = [
---
+++ user/urls.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*
+
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('', views.index),
+]
